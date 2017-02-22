aeb30d51389d4c72040dd77a47109c064bda3d8e
Zan Smirnov <c15437072@mydit.ie>
2017-02-22 19:08:15 +0000

    Camera Shake

---
+++ BluRaii/Assets/Scripts/Camera_Shake.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Shake : MonoBehaviour {
+    Camera mainCam;
+    List<Shake> shakes = new List<Shake>();
+
+	// Use this for initialization
+	void Start () {
+        mainCam = Camera.main;
+        AddShake(10, 1000000);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        foreach(Shake shake in shakes) {
+            if(shake.startTime + shake.duration > Time.time) {
+                Quake(shake);
+            } else {
+                if(!shake.finished) {
+                    mainCam.transform.position = shake.originalPosition;
+                }
+
+                shake.finished = true;
+            }
+        }
+    }
+
+    void Quake(Shake shake) {
+        Vector3 randomVector = Vector3.zero;
+
+        if (transform.localPosition.x > shake.originalPosition.x) {
+            randomVector.x = Random.Range(-shake.intensity, shake.intensity);
+        } else {
+            randomVector.x = Random.Range(-shake.intensity, shake.intensity);
+        }
+
+        if (transform.localPosition.y > shake.originalPosition.y) {
+            randomVector.y = Random.Range(-shake.intensity, shake.intensity);
+        } else {
+            randomVector.y = Random.Range(-shake.intensity, shake.intensity);
+        }
+
+        if (transform.localPosition.z > shake.originalPosition.z) {
+            randomVector.z = Random.Range(-shake.intensity, shake.intensity);
+        } else {
+            randomVector.z = Random.Range(-shake.intensity, shake.intensity);
+        }
+
+        mainCam.transform.localPosition = shake.originalPosition + randomVector;
+    } 
+
+    public void AddShake(float intensity, float duration) {
+        Shake shake = new Shake();
+
+        shake.originalPosition = mainCam.transform.localPosition;
+        shake.intensity = intensity;
+        shake.duration = duration;
+        shake.startTime = Time.time;
+
+        shakes.Add(shake);
+    }
+
+    class Shake {
+        public float intensity;
+        public float startTime;
+        public float duration;
+        public bool finished = false;
+
+        public Vector3 originalPosition;
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# BluRaii
+# BluRaii
 
 #### Description
 A racing game that goes by the name of BlueRaii which is currently in development. This game will be powered by Unity3D engine and written in C#.
