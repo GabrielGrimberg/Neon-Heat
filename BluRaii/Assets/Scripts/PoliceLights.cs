7d676773be9417c53dedd6c8b2ae0b78bf7dccd0
Zan Smirnov <c15437072@mydit.ie>
2017-02-22 18:30:21 +0000

    Police Lights

---
+++ BluRaii/Assets/Scripts/PoliceLights.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoliceLights : MonoBehaviour {
+    public float rotationsPerSecond = 2;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(0, (rotationsPerSecond * 360.0f) * Time.deltaTime, 0));
+	}
+}
