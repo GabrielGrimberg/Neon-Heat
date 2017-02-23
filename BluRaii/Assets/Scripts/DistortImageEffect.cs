55b7a693b6eb809f36f9def158a3c64754156b15
Zan Smirnov <c15437072@mydit.ie>
2017-02-23 14:00:44 +0000

    Added invert shader

---
+++ BluRaii/Assets/Scripts/DistortImageEffect.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistortImageEffect : MonoBehaviour {
+    Material mat;
+    public Shader shader;
+    
+    // Use this for initialization
+    void Start () {
+        mat = new Material(shader);
+        mat.name = "ImageEffectMaterial";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnRenderImage(RenderTexture src, RenderTexture dst) {
+        if (shader && mat) {
+            Graphics.Blit(src, dst, mat);
+        } else {
+            Graphics.Blit(src, dst);
+        }
+    }
+}
