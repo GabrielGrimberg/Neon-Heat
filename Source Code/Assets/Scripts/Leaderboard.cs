4478a792f4eb9fcbd76695c32a2f450e43695f94
Gabriel Grimberg <gabby.gr97@hotmail.com>
2017-03-02 10:29:28 +0000

    Improved Leaderboard & UI

--- Source Code/Assets/Scripts/Leaderboard.cs
+++ Source Code/Assets/Scripts/Leaderboard.cs
@@ -9,14 +9,18 @@ public class Leaderboard : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         try {
-            client = new TcpClient("localhost", 9999);
+            client = new TcpClient("192.168.1.14", 9999);
         } catch (SocketException e) {
             Debug.Log("Connection to leaderboard server couldn't be made.");
         }
 
         //SubmitScore ("Evgeniy", 132);
         //SubmitScore ("Gabz", -5);
-        Debug.Log(ReceiveScore());
+        //SubmitScore("Daniel", 2344);
+        //SubmitScore("Sportsmen", 999999);
+        //SubmitScore("Evgeniy", 132);
+        //SubmitScore("Gabz", -5);
+        //SubmitScore("Abdul", 123123);
 	}
 	
 	// Update is called once per frame
@@ -45,4 +49,31 @@ public class Leaderboard : MonoBehaviour {
 
 		return responseData;
 	}
+
+    public List<PlayerScore> GetPlayerScore() {
+        List<PlayerScore> playerScores = new List<PlayerScore>();
+
+        string data = ReceiveScore();
+        string[] commaSplit = data.Split(',');
+
+        foreach (string namescore in commaSplit) {
+            if (namescore == "###") {
+                continue;
+            }
+
+            playerScores.Add(new PlayerScore(namescore.Split('#')[0], int.Parse(namescore.Split('#')[1])));
+        }
+
+        return playerScores;
+    } 
+
+    public class PlayerScore {
+        public string player;
+        public int score;
+
+        public PlayerScore(string player, int score) {
+            this.player = player;
+            this.score = score;
+        }
+    }
 }
