d9154b2d84dfdf95a3e7198188daa622af153f5f
Gabriel Grimberg <gabby.gr97@hotmail.com>
2017-03-03 19:44:18 +0000

    Desert Progress & Some Tweaks

---
+++ Source Code/Neon Heat/Assets/Scripts/PortalDesertSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalDesertSpawner : MonoBehaviour {
+    public GameObject entrancePortal;
+    public GameObject exitPortal;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void EnterDesert() {
+        entrancePortal.transform.position = exitPortal.transform.position + Vector3.back * Random.Range(100000, 200000) + Vector3.right * Random.Range(-10000.0f, 10000.0f);
+    }
+}
