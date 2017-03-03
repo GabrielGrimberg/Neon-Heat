8b72c94267a85e39be14964e953528c585481a62
EvgenyTimoshin <evgenytimoshin@gmail.com>
2017-03-03 19:27:56 +0000

    Neon Heat Android Application

---
+++ Neon Heat App/Assets/UDPReceive.cs
@@ -0,0 +1,136 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+public class UDPReceive : MonoBehaviour
+{
+
+    // receiving Thread
+    Thread receiveThread;
+
+    // udpclient object
+    UdpClient client;
+
+    // public
+    public string IP = "192.168.1.12";
+    public int port; // define > init
+
+    // infos
+    public static string lastReceivedUDPPacket = "";
+    public string allReceivedUDPPackets = ""; // clean up this from time to time!
+
+
+    
+
+    // start from shell
+    private static void Main()
+    {
+        UDPReceive receiveObj = new UDPReceive();
+        receiveObj.init();
+
+        string text = "";
+        do
+        {
+            text = Console.ReadLine();
+        }
+        while (!text.Equals("exit"));
+    }
+    // start from unity3d
+    public void Start()
+    {
+
+        init();
+    }
+
+    // OnGUI
+    void OnGUI()
+    {
+        Rect rectObj = new Rect(40, 10, 200, 400);
+        GUIStyle style = new GUIStyle();
+        style.alignment = TextAnchor.UpperLeft;
+        GUI.Box(rectObj, "# UDPReceive\n127.0.0.1 " + port + " #\n"
+                    + "shell> nc -u 127.0.0.1 : " + port + " \n"
+                    + "\nLast Packet: \n" + lastReceivedUDPPacket
+                    + "\n\nAll Messages: \n" + allReceivedUDPPackets
+                , style);
+    }
+
+    // init
+    private void init()
+    { 
+        print("UDPSend.init()");
+
+        // define port
+        port = 15000;
+
+        // status
+        print("Sending to 127.0.0.1 : " + port);
+        print("Test-Sending to this Port: nc -u 127.0.0.1  " + port + "");
+
+        receiveThread = new Thread(
+            new ThreadStart(ReceiveData));
+        receiveThread.IsBackground = true;
+        receiveThread.Start();
+
+    }
+
+    // receive thread
+    private void ReceiveData()
+    {
+
+        client = new UdpClient(port);
+        while (true)
+        {
+
+            try
+            {
+
+               
+                IPEndPoint anyIP = new IPEndPoint(IPAddress.Any, 0);
+                byte[] data = client.Receive(ref anyIP);
+
+                string text = Encoding.UTF8.GetString(data);
+
+                if (text == "crashed"){
+                    Handheld.Vibrate();
+                }
+
+                if (text == "powerup") {
+                    AudioSource powerup = GetComponent<AudioSource>();
+                    powerup.Play();
+                }
+
+               
+                print(">> " + text);
+
+                // latest UDPpacket
+                lastReceivedUDPPacket = text;
+
+                // ....
+                allReceivedUDPPackets = allReceivedUDPPackets + text;
+
+            }
+            catch (Exception err)
+            {
+                print(err.ToString());
+            }
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        client.Close();
+        receiveThread.Abort();
+    }
+
+    public string getLatestUDPPacket()
+    {
+        allReceivedUDPPackets = "";
+        return lastReceivedUDPPacket;
+    }
+}
